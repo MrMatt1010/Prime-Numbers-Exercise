b09dd6ed497cef1811ff7ca8e9254206ed7d8a94
Matt T <mrsymtec@gmail.com>
2023-09-26 17:22:27 +1300

    Started adding the code to the file, beginning with the prime number calculations

--- Prime-Numbers/Prime-Numbers.cpp
+++ Prime-Numbers/Prime-Numbers.cpp
@@ -1,9 +1,25 @@
 // Prime-Numbers.cpp :
 
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    
+
+bool is_prime(int num) {
+    if (num <= 1)
+        return false;
+    if (num <= 3)
+        return true;
+    if (num % 2 == 0 || num % 3 == 0)
+        return false;
+
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0)
+            return false;
+    }
+    return true;
+
+int main(){
+    int start, end;
+
 }
 
