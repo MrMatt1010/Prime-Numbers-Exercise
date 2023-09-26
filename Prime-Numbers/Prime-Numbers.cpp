1577e3173a9c48e91f2d53ec4bec52f867ae8dc5
Matt T <mrsymtec@gmail.com>
2023-09-26 17:42:14 +1300

    Completed codebase, now need to add comments and get the output to match what is expected.

--- Prime-Numbers/Prime-Numbers.cpp
+++ Prime-Numbers/Prime-Numbers.cpp
@@ -17,9 +17,46 @@ bool is_prime(int num) {
             return false;
     }
     return true;
-
-int main(){
-    int start, end;
-
 }
+    int main() {
+        int start, end;
+        std::vector<int> divisible_by_7;
+        std::vector<int> prime_numbers;
 
+        std::cout << "Enter the starting Number: ";
+        std::cin >> start;
+
+        std::cout << "Enter the ending Number: ";
+        std::cin >> end;
+
+        if (start > end) {
+            std::cout << "Starting number should be less than the ending number." << std::endl;
+        }
+        else {
+            std::cout << "Numbers divisible by 7 in the range(" << start << " to " << end << "): ";
+            for (int num = start; num <= end; num++) {
+                if (num % 7 == 0) {
+                    divisible_by_7.push_back(num);
+                }
+            }
+
+            for (int num = start; num <= end; num++) {
+                if (is_prime(num)) {
+                    prime_numbers.push_back(num);
+                }
+            }
+
+            std::cout << "Divisible by 7: ";
+            for (int num : prime_numbers) {
+                std::cout << num << " ";
+            }
+
+            std::cout << "\nPrime Numbers: ";
+            for (int num : prime_numbers) {
+                std::cout << num << " ";
+            }
+            std::cout << "\nTotal prime numbers in the range: " << prime_numbers.size() << std::endl;
+        }
+
+        return(0);
+    }
